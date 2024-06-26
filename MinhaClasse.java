471f2f461f6ee6e5d661ab42641e1b5fd35e6247
caiquemiranda <caiquemirandas@gmail.com>
2024-06-26 19:39:59 -0300

    add `.gitignore`

--- MinhaClasse.java
+++ MinhaClasse.java
@@ -1,7 +1,7 @@
 
-public class MinhaClasse {
+class MinhaClasse {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         System.out.println("Hello World !");
 
