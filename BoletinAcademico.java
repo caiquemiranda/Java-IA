db38c0dec0ad2bfabd90891f4861feb69173373f
caiquemiranda <caiquemirandas@gmail.com>
2024-06-26 19:59:45 -0300

    corr code

---
+++ BoletinAcademico.java
@@ -0,0 +1,7 @@
+public class BoletinAcademico{
+    public static void main(String[] args) {
+        int = mediaFinal = 6;
+
+        if(meda
+        else if(mediaFinal >= 5 && mediaFinal < 6))
+}
