7d4e9dc5ee5530575e1fe8190265b842a867ffb2
caiquemiranda <caiquemirandas@gmail.com>
2024-07-01 20:10:42 -0300

    flow-control

---
+++ edu/controle-fluxo/controle-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,11 @@
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 26.0;
+
+        if (valorSolicitado <= saldo) {
+            System.out.println("Saque efetuado com sucesso!");
+            saldo -= valorSolicitado;
+        }
+    }
+}
