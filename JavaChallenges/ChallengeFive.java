f6f33e376044a2cebaf36a56c77d612df3af5baa
caiquemiranda <caiquemirandas@gmail.com>
2024-07-03 09:53:18 -0300

    update `README.md`

--- JavaChallenges/ChallengeFive.java
+++ JavaChallenges/ChallengeFive.java
@@ -1,5 +1,42 @@
 package JavaChallenges;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class ChallengeFive {
-    
+
+    public class ControleSimplesDeSaques {
+
+        public static void main(String[] args) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            try {
+
+                double limiteDiario = Double.parseDouble(br.readLine());
+
+                for (int i = 1;; i++) {
+                    double valorSaque = Double.parseDouble(br.readLine());
+
+                    if (valorSaque == 0) {
+                        System.out.println("Transacoes encerradas.");
+                        break;
+                    } else if (valorSaque > limiteDiario) {
+                        System.out.println("Limite diario de saque atingido. Transacoes encerradas.");
+                        break;
+                    } else {
+
+                        limiteDiario -= valorSaque;
+                        System.out.printf("Saque realizado. Limite restante: %.1f\n", limiteDiario);
+                    }
+                }
+            } catch (IOException e) {
+
+                System.out.println("Erro de I/O: " + e.getMessage());
+            } catch (NumberFormatException e) {
+                System.out.println("Erro: Entrada invalida. Por favor, insira um numero valido.");
+            }
+        }
+    }
+
 }
--- JavaChallenges/ChallengerFour.java
+++ JavaChallenges/ChallengerFour.java
@@ -1,5 +1,45 @@
 package JavaChallenges;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class ChallengerFour {
+
+    public class VerificacaoChequeEspecial {
+
+        public static void main(String[] args) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            try {
+
+                double saldo = Double.parseDouble(br.readLine());
+
+                double saque = Double.parseDouble(br.readLine());
+
+                double limiteChequeEspecial = 500;
+
+                if (saque <= saldo) {
+
+                    System.out.println("Transacao realizada com sucesso.");
+                } else {
+
+                    if (saque <= saldo + limiteChequeEspecial) {
+
+                        System.out.println("Transacao realizada com sucesso utilizando o cheque especial.");
+                    } else {
+
+                        System.out.println("Transacao nao realizada. Limite do cheque especial excedido.");
+                    }
+                }
+            } catch (IOException e) {
+
+                System.out.println("Erro de I/O: " + e.getMessage());
+            } catch (NumberFormatException e) {
+
+                System.out.println("Erro: Entrada invalida. Por favor, insira um numero valido.");
+            }
+        }
+    }
 
 }
